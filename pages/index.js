0ee4d41455c156a43e3ed7f9909e56b3dfa9b2ce
Karpov Yuri <yurick2@bk.ru>
2022-01-15 14:45:27 +0300

    feat: Добавить 6 карточек при загрузке страницы

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,33 @@
+const initialCards = [{
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+let cardPicture;
+let cardTitle;
+const cardsContainer = document.querySelector('.cards');
+
 const profile = document.querySelector('.profile');
 const editButton = profile.querySelector('.profile__edit-button');
 let profileName = profile.querySelector('.profile__name');
@@ -8,6 +38,7 @@ const closeButton = popup.querySelector('.popup__close-button');
 let nameInput = popup.querySelector('#nameInput');
 let jobInput = popup.querySelector('#jobInput');
 
+//Функция открытия модального окна
 
 function popupOpen() {
   nameInput.value = profileName.textContent;
@@ -15,9 +46,13 @@ function popupOpen() {
   popup.classList.add('popup_opened');
 }
 
+//Функция закрытия модального окна
+
 function popupClose() {
   popup.classList.remove('popup_opened');
 }
+
+// Функция изменения данных профиля
 
 function formSubmit(evt) {
   evt.preventDefault();
@@ -25,6 +60,31 @@ function formSubmit(evt) {
   profileJob.textContent = jobInput.value;
   popupClose();
 }
+
+// Функция добавления карточки
+
+function addCard() {
+  cardsContainer.innerHTML = `
+    <div class="card">
+      <img src="${cardPicture}" alt="${cardTitle}" class="card__picture">
+      <div class="card__info">
+        <h2 class="card__title">${cardTitle}</h2>
+        <img src="images/heart_disabled.svg" alt="Иконка сердечка" class="card__like">
+      </div>
+    </div>
+  ` + cardsContainer.innerHTML;
+}
+
+
+// Вывод начальных 6 карточек
+
+for (let i = 5; i >= 0; i--) {
+  cardPicture = initialCards[i].link;
+  cardTitle = initialCards[i].name;
+  addCard();
+}
+
+// Слушатели открытия и закрытия модального окна
 
 editButton.addEventListener('click', popupOpen);
 closeButton.addEventListener('click', popupClose);
