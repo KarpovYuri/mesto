6ef45d615a52138962992f0e454dfcd0ac661ffb
Karpov Yuri <yurick@bk.ru>
2022-01-25 23:18:03 +0300

    Проектная работа 5: Отправить на проверку

--- pages/index.js
+++ pages/index.js
@@ -27,14 +27,15 @@ const initialCards = [{
 ];
 
 
-// Переменные карточек
+// Переменные данных карточки
 
 let cardPicture;
 let cardTitle;
 const cardsContainer = document.querySelector('.cards');
+const cardTemplate = document.querySelector('#card-template').content;
 
 
-// Переменные профиля
+// Переменные данных профиля
 
 const profile = document.querySelector('.profile');
 const editButton = profile.querySelector('.profile__edit-button');
@@ -79,13 +80,14 @@ function popupOpen() {
     case 'add-button':
       popupAdd.classList.add('popup_opened');
       break;
-    default:
+    case 'card-image':
       imageTeg.src = this.src;
       imageTeg.alt = this.alt;
       signatureTeg.textContent = this.alt;
       popupImage.classList.add('popup_opened');
   }
 }
+
 
 // Закрытие popup'ов
 
@@ -99,12 +101,11 @@ function popupClose() {
       break;
     case 'image-close':
       popupImage.classList.remove('popup_opened');
-      break;
   }
 }
 
 
-// Сохранение данных профиля
+// Добавление данных профиля
 
 function editFormSubmit(evt) {
   evt.preventDefault();
@@ -114,7 +115,7 @@ function editFormSubmit(evt) {
 }
 
 
-// Сохранение данных карточки
+// Добавление пользовательской карточки
 
 function addFormSubmit(evt) {
   evt.preventDefault();
@@ -122,57 +123,24 @@ function addFormSubmit(evt) {
   cardPicture = pictureInput.value;
   addCard();
   popupAdd.classList.remove('popup_opened');
-
-  // Переназначение обрботчиков событий изображениям, кнопкам 'like' и 'trash' после добавления карточки
-  pictures = cardsContainer.querySelectorAll('.card__picture');
-  likes = cardsContainer.querySelectorAll('.card__like');
-  trashes = cardsContainer.querySelectorAll('.card__trash');
-  setButtonEventListener();
 }
 
 
-// Добавление HTML-кода карточки
+// Вывод карточки на страницу и назначение обработчиков
 
 function addCard() {
-  cardsContainer.innerHTML = `
-    <div class="card">
-      <button type="button" aria-label="Иконка мусорного бака" class="card__trash fade-opacity"></button>
-      <img src="${cardPicture}" alt="${cardTitle}" class="card__picture">
-      <div class="card__info">
-        <h2 class="card__title">${cardTitle}</h2>
-        <button type="button" aria-label="Иконка сердечка" class="card__like"></button>
-      </div>
-    </div>
-  ` + cardsContainer.innerHTML;
-}
-
-
-// Удаление карточки
-
-function removeCard() {
-  this.parentElement.remove();
-}
-
-
-// Добавление/Удаление лайка
-
-function addLike() {
-  if (this.classList[this.classList.length - 1] === 'card__like_active') {
-    this.classList.remove('card__like_active');
-  } else {
-    this.classList.add('card__like_active');
-  }
-}
-
-
-// Перебор изображений, кнопок 'like' и 'trash' с назначение обработчиков
-
-function setButtonEventListener() {
-  for (let i = 0; i < pictures.length; i++) {
-    pictures[i].addEventListener('click', popupOpen);
-    likes[i].addEventListener('click', addLike);
-    trashes[i].addEventListener('click', removeCard);
-  }
+  let cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  cardElement.querySelector('.card__picture').src = cardPicture;
+  cardElement.querySelector('.card__picture').alt = cardTitle;
+  cardElement.querySelector('.card__title').textContent = cardTitle;
+  cardElement.querySelector('.card__like').addEventListener('click', (evt) => {
+    evt.target.classList.toggle('card__like_active');
+  });
+  cardElement.querySelector('.card__trash').addEventListener('click', (evt) => {
+    evt.target.parentElement.remove();
+  });
+  cardElement.querySelector('.card__picture').addEventListener('click', popupOpen);
+  cardsContainer.prepend(cardElement);
 }
 
 
@@ -182,25 +150,15 @@ for (let i = 5; i >= 0; i--) {
   cardPicture = initialCards[i].link;
   cardTitle = initialCards[i].name;
   addCard();
-}
+};
 
 
-// Назначение обрботчиков событий изображениям, кнопкам 'like' и 'trash'
-
-let pictures = cardsContainer.querySelectorAll('.card__picture');
-let likes = cardsContainer.querySelectorAll('.card__like');
-let trashes = cardsContainer.querySelectorAll('.card__trash');
-setButtonEventListener();
-
-
-// Назначение обрботчиков событий остальных кнопок
+// Назначение обрботчиков событий кнопкам
 
 editButton.addEventListener('click', popupOpen);
 editCloseButton.addEventListener('click', popupClose);
 popupEdit.addEventListener('submit', editFormSubmit);
-
 addButton.addEventListener('click', popupOpen);
 addCloseButton.addEventListener('click', popupClose);
 popupAdd.addEventListener('submit', addFormSubmit);
-
 imageCloseButton.addEventListener('click', popupClose);
