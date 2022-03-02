09fda8a7ded7e376e486131ed17a563f409816ee
Karpov Yuri <yurick2@bk.ru>
2022-03-02 22:21:33 +0300

    fix: Исправить замечания после ревью

---
+++ js/constants.js
@@ -0,0 +1,3 @@
+export const popupImage = document.querySelector('#popup-image');
+export const popupPicture = popupImage.querySelector('.popup__image');
+export const popupSignature = document.querySelector('.popup__signature');
