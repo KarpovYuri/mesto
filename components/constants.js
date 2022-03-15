85e6fcfc654d72bc5871f04d28ade369922a1972
Karpov Yuri <yurick2@bk.ru>
2022-03-15 20:15:13 +0300

    fix: слияние с веткой develop

--- components/constants.js
+++
@@ -1,3 +0,0 @@
-export const popupImage = document.querySelector('#popup-image');
-export const popupPicture = popupImage.querySelector('.popup__image');
-export const popupSignature = document.querySelector('.popup__signature');
