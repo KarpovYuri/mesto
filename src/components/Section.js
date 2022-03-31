525b93caac442ee631c044a59ffd4f9698bef97c
Karpov Yuri <yurick2@bk.ru>
2022-03-31 23:36:07 +0300

    feat: Add loading of initial cards

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,15 +1,14 @@
 // Класс создания и вывода карточек на страницу
 export default class Section {
-  constructor({ items, renderer }, containerSelector) {
-    this._renderedItems = items; // Массив данных карточек
+  constructor({ renderer }, containerSelector) {
     this._renderer = renderer; // Функция формирования карточки
     this._container = document.querySelector(containerSelector); // контейнер карточек
   }
 
 
   // Отрисовка всех карточек
-  renderItems() {
-    this._renderedItems.reverse().forEach((item) => {
+  renderItems(items) {
+    items.reverse().forEach((item) => {
       this.render(item);
     });
   }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,32 +1,3 @@
-// Данные начальных карточек
-
-export const initialCards = [{
-  name: 'Архыз',
-  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-},
-{
-  name: 'Челябинская область',
-  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-},
-{
-  name: 'Иваново',
-  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-},
-{
-  name: 'Камчатка',
-  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-},
-{
-  name: 'Холмогорский район',
-  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-},
-{
-  name: 'Байкал',
-  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-}
-];
-
-
 // Объект классов необходимый для запуса валидации
 export const formClasses = {
   formSelector: '.popup__form',
