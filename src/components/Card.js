eb9447e112215a8d2c9d3f588f5ab22207037314
Karpov <yurick@bk.ru>
2023-12-11 16:02:52 +0300

    fix: снятие/постановку лайка

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ export default class Card {
     cardSelector,
     handleImageClick,
     handleTrashClick,
-    handleLike
+    handleLike,
   }) {
     this._data = data; // данные карточки
     this._userId = userId; // id пользователя
@@ -15,23 +15,20 @@ export default class Card {
     this._handleImageClick = handleImageClick; // функция открытия popup'а изображения
     this._handleTrashClick = handleTrashClick; // функция открытия popup'а изображения
     this._handleLike = handleLike; // функция лайка
-    this._cardPicture = this._cardElement.querySelector('.card__picture'); // изображение карточки
-    this._trashBtn = this._cardElement.querySelector('.card__trash'); // кнопка удаления
-    this._likeBtn = this._cardElement.querySelector('.card__like-btn'); // Cчетчик лайков
-    this._likeQty = this._cardElement.querySelector('.card__like-qty'); // Cчетчик лайков
+    this._cardPicture = this._cardElement.querySelector(".card__picture"); // изображение карточки
+    this._trashBtn = this._cardElement.querySelector(".card__trash"); // кнопка удаления
+    this._likeBtn = this._cardElement.querySelector(".card__like-btn"); // Cчетчик лайков
+    this._likeQty = this._cardElement.querySelector(".card__like-qty"); // Cчетчик лайков
   }
-
 
   // Получение шаблона разметки
   _getTemplate() {
     const cardElement = document
       .querySelector(this._cardSelector)
-      .content
-      .querySelector('.card')
+      .content.querySelector(".card")
       .cloneNode(true);
     return cardElement;
   }
-
 
   // Получение ID карточки
   getId() {
@@ -40,42 +37,41 @@ export default class Card {
 
   // Изменение состояния лайка
   updateLikes(data) {
+    this._data = data;
     this._likeQty.textContent = data.likes.length;
-    this._likeBtn.classList.toggle('card__like-btn_active');
+    this._likeBtn.classList.toggle("card__like-btn_active");
   }
-
 
   // Установка обработчиков событий карточки
   _setEventListeners() {
-
     // Установка обработчика событий кнопке 'Like'
-    this._cardElement.querySelector('.card__like-btn')
-      .addEventListener('click', () => {
+    this._cardElement
+      .querySelector(".card__like-btn")
+      .addEventListener("click", () => {
         this._handleLike(this);
       });
 
     // Установка обработчика событий кнопке удаления карточки
     if (this._data.owner._id === this._userId) {
-      this._trashBtn.classList.add('card__trash_active');
-      this._trashBtn.addEventListener('click', () => {
+      this._trashBtn.classList.add("card__trash_active");
+      this._trashBtn.addEventListener("click", () => {
         this._handleTrashClick(this);
       });
     }
 
     // Установка обработчика событий изображению
-    this._cardPicture.addEventListener('click', this._handleImageClick);
-
+    this._cardPicture.addEventListener("click", this._handleImageClick);
   }
-
 
   // Создание карточки
   createCard() {
     this._cardPicture.src = this._data.link;
     this._cardPicture.alt = this._data.name;
-    this._cardElement.querySelector('.card__title').textContent = this._data.name;
+    this._cardElement.querySelector(".card__title").textContent =
+      this._data.name;
     this._likeQty.textContent = this._data.likes.length;
-    if (this._data.likes.some(item => item._id === this._userId)) {
-      this._likeBtn.classList.add('card__like-btn_active');
+    if (this._data.likes.some((item) => item._id === this._userId)) {
+      this._likeBtn.classList.add("card__like-btn_active");
     }
     this._setEventListeners();
     return this._cardElement;
@@ -84,7 +80,6 @@ export default class Card {
   // Удаление карточки
   deleteCard() {
     this._cardElement.remove();
-    this._cardElement = '';
+    this._cardElement = "";
   }
-
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 // Импорт главного файла стилей
-import './index.css';
-
+import "./index.css";
 
 // Импорт классов
 import Card from "../components/Card.js";
@@ -12,7 +11,6 @@ import PopupWithConfirmation from "../components/PopupWithConfirmation.js";
 import UserInfo from "../components/UserInfo.js";
 import Api from "../components/Api.js";
 
-
 // Импорт переменных
 import {
   cardConteinerSelector,
@@ -23,26 +21,22 @@ import {
   profileAbout,
   profileAvatar,
   formClasses,
-  formValidators
-} from '../utils/constants.js';
-
+  formValidators,
+} from "../utils/constants.js";
 
 // Переменная для id пользователя
 let userId;
-
 
 // Функция изменения текста кнопки на: «Сохранить»
 function removeSaving(btn) {
   btn.textContent = "Сохранить";
 }
 
-
 // Создание экземпляра класса общения с сервером
 const api = new Api({
-  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-39/',
-  token: 'e7a7c3fb-6194-4371-9a2c-b0a475e73e1c'
+  baseUrl: "https://mesto.nomoreparties.co/v1/cohort-39/",
+  token: "e7a7c3fb-6194-4371-9a2c-b0a475e73e1c",
 });
-
 
 // Начальная отрисовка данных профиля и карточек
 Promise.all([api.getUserInfo(), api.getInitialCards()])
@@ -51,114 +45,120 @@ Promise.all([api.getUserInfo(), api.getInitialCards()])
     userInfoInstance.setUserInfo(userData);
     cardsList.renderItems(cards);
   })
-  .catch(error => console.log(error));
-
+  .catch((error) => console.log(error));
 
 // Создание экземпляра класса данных пользователя
 const userInfoInstance = new UserInfo(profileName, profileAbout, profileAvatar);
 
-
 // Создание эксземпляра класса popup'а данных пользоввателя
 const editPopup = new PopupWithForm({
-  popupSelector: '#popup-edit',
+  popupSelector: "#popup-edit",
   submitCallback: (formItems) => {
     editPopup.renderLoading();
-    api.addUserInfo(formItems)
-      .then(result => {
+    api
+      .addUserInfo(formItems)
+      .then((result) => {
         userInfoInstance.setUserInfo(result);
         editPopup.closePopup();
       })
-      .catch(error => console.log(error))
+      .catch((error) => console.log(error))
       .finally(() => {
-        editPopup.renderLoading('Сохранить');
+        editPopup.renderLoading("Сохранить");
       });
-  }
+  },
 });
-
 
 // Создание эксземпляра класса popup'а обновления аватара
 const avatarPopup = new PopupWithForm({
-  popupSelector: '#popup-avatar',
+  popupSelector: "#popup-avatar",
   submitCallback: (formItems) => {
     avatarPopup.renderLoading();
-    api.updateAvatar(formItems)
-      .then(result => {
+    api
+      .updateAvatar(formItems)
+      .then((result) => {
         userInfoInstance.setUserInfo(result);
         avatarPopup.closePopup();
       })
-      .catch(error => console.log(error))
+      .catch((error) => console.log(error))
       .finally(() => {
-        avatarPopup.renderLoading('Сохранить');
+        avatarPopup.renderLoading("Сохранить");
       });
-  }
+  },
 });
 
-
 // Создание эксземпляра класса popup'а изображения
-const imagePopup = new PopupWithImage('#popup-image');
-
+const imagePopup = new PopupWithImage("#popup-image");
 
 // Создание эксземпляра класса popup'а подтверждения удаления
 const deletePopup = new PopupWithConfirmation({
-  popupSelector: '#popup-delete',
+  popupSelector: "#popup-delete",
   submitCallback: (data) => {
-    api.deleteCard(data.getId())
+    api
+      .deleteCard(data.getId())
       .then(() => {
         data.deleteCard();
         deletePopup.closePopup();
       })
-      .catch(error => console.log(error));
-  }
+      .catch((error) => console.log(error));
+  },
 });
-
 
 // Функция создание экземпляра карточки
 function createCard(elem) {
   const cardInstance = new Card({
     data: elem,
     userId: userId,
-    cardSelector: '#card-template',
+    cardSelector: "#card-template",
     handleImageClick: (e) => imagePopup.openPopup(e),
     handleTrashClick: (data) => {
       deletePopup.data = data;
       deletePopup.openPopup();
     },
     handleLike: (data) => {
-      api.setCardLike(data.getId())
-        .then(result => data.updateLikes(result))
-        .catch(error => console.log(error));
-    }
+      if (data._data.likes.some((item) => item._id === userId)) {
+        api
+          .removeCardLike(data.getId())
+          .then((result) => data.updateLikes(result))
+          .catch((error) => console.log(error));
+      } else {
+        api
+          .setCardLike(data.getId())
+          .then((result) => data.updateLikes(result))
+          .catch((error) => console.log(error));
+      }
+    },
   });
   return cardInstance;
 }
 
-
 // Создание экземпляра класса секции карточек
-const cardsList = new Section({
-  renderer: (elem) => {
-    const card = createCard(elem);
-    return card.createCard();
-  }
-}, cardConteinerSelector);
-
+const cardsList = new Section(
+  {
+    renderer: (elem) => {
+      const card = createCard(elem);
+      return card.createCard();
+    },
+  },
+  cardConteinerSelector
+);
 
 // Добавление пользовательской карточки
 const addPopup = new PopupWithForm({
-  popupSelector: '#popup-add',
+  popupSelector: "#popup-add",
   submitCallback: (formItems) => {
     addPopup.renderLoading();
-    api.addCard(formItems)
-      .then(result => {
+    api
+      .addCard(formItems)
+      .then((result) => {
         cardsList.render(result);
         addPopup.closePopup();
       })
-      .catch(error => console.log(error))
+      .catch((error) => console.log(error))
       .finally(() => {
-        addPopup.renderLoading('Создать');
+        addPopup.renderLoading("Создать");
       });
-  }
+  },
 });
-
 
 // Установка обработчиков событий крестикам и оверлеям popap'ов
 editPopup.setEventListeners();
@@ -167,41 +167,38 @@ imagePopup.setEventListeners();
 deletePopup.setEventListeners();
 avatarPopup.setEventListeners();
 
-
 // Назначение обрботчиков событий кнопке формы профиля
-editButton.addEventListener('click', () => {
+editButton.addEventListener("click", () => {
   const userData = userInfoInstance.getUserInfo();
   editPopup.setInputValues(userData);
   formValidators.profileForm.resetValidation();
   editPopup.openPopup();
 });
 
-
 // Назначение обрботчиков событий кнопке формы добавления карточки
-addButton.addEventListener('click', () => {
+addButton.addEventListener("click", () => {
   formValidators.addForm.resetValidation();
   addPopup.openPopup();
 });
 
-
 // Назначение обрботчиков событий кнопке формы обновления аватара
-avatarButton.addEventListener('click', () => {
+avatarButton.addEventListener("click", () => {
   formValidators.avatarForm.resetValidation();
   avatarPopup.openPopup();
 });
 
-
 // Функция запуска валидации
 function enableValidation(formClasses) {
-  const formList = Array.from(document.querySelectorAll(formClasses.formSelector));
+  const formList = Array.from(
+    document.querySelectorAll(formClasses.formSelector)
+  );
   formList.forEach((formElement) => {
     const validator = new FormValidator(formClasses, formElement);
-    const formName = formElement.getAttribute('name');
+    const formName = formElement.getAttribute("name");
     formValidators[formName] = validator;
     validator.enableValidation();
   });
 }
-
 
 // Включение валидации
 enableValidation(formClasses);
